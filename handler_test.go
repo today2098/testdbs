c5966629f540ead33d719c4503353551f71bfe72
Today <76273733+today2098@users.noreply.github.com>
2024-12-04 14:51:18 +0900

    [add] Create an example for "NewHandler()"

---
+++ handler_test.go
@@ -0,0 +1,16 @@
+package testdbs_test
+
+import "github.com/today2098/testdbs"
+
+func ExampleNewHandler() {
+	dsn := "user:password@tcp(localhost:3306)/?multiStatements=true"
+	sourceUrl := "file:///home/path/to/your/migrations"
+	h := testdbs.NewHandler("mysql", dsn, sourceUrl)
+	defer h.Close()
+
+	td, _ := h.CreateAndMigrate()
+	defer td.Drop()
+
+	dbx := td.DBx()
+	dbx.Exec("INSERT INTO tables (id) VALUES (1)")
+}
